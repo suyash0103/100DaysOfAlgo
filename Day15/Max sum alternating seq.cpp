2a5caac3b9135be7bd6ffc0a986fe201d3976131
suyash0103 <suyashghuge@gmail.com>
2020-09-26 00:09:45 +0530

    alternating seq

---
+++ Day15/Max sum alternating seq.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	vector<int> v(n);
+	for(int i = 0; i < n; i++)
+		cin >> v[i];
+	
+	vector<int> lis(n, 1);
+	vector<vector<int> > sum(n, vector<int>(2, 1));
+	
+	for(int i = 1; i < n; i++)
+	{
+//		cout << "i : " << i << endl;
+		for(int j = 0; j < i; j++)
+		{
+//			cout << j << " ";
+			if(j == 0 && v[i] < v[j])
+			{
+				sum[i][0] = max(sum[i][0], v[i] + v[j]);
+			}
+			else if(j != 0)
+			{
+				if(v[i] < v[j])
+				{
+					sum[i][0] = max(sum[i][0], sum[j][1] + v[i]);
+				}
+				else
+				{
+					sum[i][1] = max(sum[i][1], sum[j][0] + v[i]);
+				}
+			}
+		}
+	}
+	int ans = 0;
+	for(int i = 0; i < n; i++)
+	{
+//		cout << sum[i][0] << " " << sum[i][1] << endl;
+		ans = max(ans, max(sum[i][0], sum[i][1]));
+	}
+	
+	cout << ans;
+}
