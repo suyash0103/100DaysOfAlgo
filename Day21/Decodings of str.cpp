f5a66ec9f9232a639d28967d94285a769316eac7
suyash0103 <suyashghuge@gmail.com>
2020-10-01 20:53:14 +0530

    decode strs

---
+++ Day21/Decodings of str.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+map<int, char> mp;
+
+set<string> st;
+
+void find(string s, string newstr, int i)
+{
+    // cout << newstr << endl;
+	if(i >= s.length())
+	{
+	   // cout << newstr;
+		if(st.find(newstr) == st.end())
+		{
+			cout << newstr << endl;
+		}
+		st.insert(newstr);
+		return;
+	}
+	
+	if(i + 1 < s.length())
+	{
+		string ss = "";
+		ss.push_back(s[i]);
+		ss.push_back(s[i + 1]);
+		int t = stoi(ss);
+// 		cout << t << endl;
+		if(t <= 26)
+		{
+			string n = newstr;
+			n.push_back(mp[t]);
+// 			cout << n << endl;
+			find(s, n, i + 2);
+		}
+	}
+	
+	string sss = "";
+	sss.push_back(s[i]);
+	int a = stoi(sss);
+	string n = newstr;
+	n.push_back(mp[a]);
+// 	cout << n << endl;
+// 	cout << a << endl;
+	find(s, n, i + 1);
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+	
+	mp[1] = 'A';
+	mp[2] = 'B';
+	mp[3] = 'C';
+	mp[4] = 'D';
+	mp[5] = 'E';
+	mp[6] = 'F';
+	mp[7] = 'G';
+	mp[8] = 'H';
+	mp[9] = 'I';
+	mp[10] = 'J';
+	mp[11] = 'K';
+	mp[12] = 'L';
+	mp[13] = 'M';
+	mp[14] = 'N';
+	mp[15] = 'O';
+	mp[16] = 'P';
+	mp[17] = 'Q';
+	mp[18] = 'R';
+	mp[19] = 'S';
+	mp[20] = 'T';
+	mp[21] = 'U';
+	mp[22] = 'V';
+	mp[23] = 'W';
+	mp[24] = 'X';
+	mp[25] = 'Y';
+	mp[26] = 'Z';
+	
+	find(s, "", 0);
+	cout << st.size();
+}
