f5f0abea3ec0c2ffd4e77308f80dab2777fb6920
suyash0103 <suyashghuge@gmail.com>
2020-10-07 00:20:47 +0530

    max dot product

--- Day26/Max dot product.cpp
+++ Day26/Max dot product.cpp
@@ -4,31 +4,48 @@ using namespace std;
 
 int ans = 0;
 
+int dp[100][100];
+
 int find(int i, int j, int zero, vector<int>& a, vector<int>& b, int sum)
 {
-	cout << i << " " << j << " " << zero << " " << sum << endl;
-	if(i == -1 && j == -1)
+	cout << i << " " << j << " " << sum << endl;
+	if(i == 0 && j == 0)
 	{
 		ans = max(ans, sum);
+		dp[i][j] = max(dp[i][j], ans);
 		return sum;
 	}
 	
-	if(zero > 0)
+	if(j == 0)
 	{
-		find(i - 1, j, zero - 1, a, b, sum + a[i] * 0);
-		if(j >= 0)
-		{
-			find(i - 1, j - 1, zero, a, b, sum + a[i] * b[j]);
-		}
+		ans = max(ans, sum);
+		dp[i][j] = max(dp[i][j], ans);
+		return sum;
 	}
-	else
+	
+//	if(dp[i][j] != -1)
+//	{
+//		return max(dp[i][j], sum);
+//	}
+	
+	int aa = -1;
+	
+	if(i > j)
 	{
-		find(i - 1, j - 1, zero, a, b, sum + a[i] * b[j]);
+		aa = max(aa, find(i - 1, j, zero, a, b, sum));
 	}
+	
+	aa = max(aa, find(i - 1, j - 1, zero, a, b, sum + a[i - 1] * b[j - 1]));
+	dp[i][j] = max(dp[i][j], aa);
+	return aa;
 }
 
 int main()
 {
+	for(int i = 0; i < 100; i++)
+		for(int j = 0; j < 100; j++)
+			dp[i][j] = -1;
+	
 	int m, n;
 	cin >> m >> n;
 	
@@ -41,6 +58,11 @@ int main()
 	for(int i = 0; i < n; i++)
 		cin >> b[i];
 	
-	find(m - 1, n - 1, m - n, a, b, 0);
+	cout << find(m, n, m - n, a, b, 0) << endl;
 	cout << ans;
 }
+
+//5
+//3
+//2 3 1 7 8
+//3 6 7
