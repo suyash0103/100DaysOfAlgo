ed97ea0e1dda5f0267fc8751e5d61e72b26e43d3
suyash0103 <suyashghuge@gmail.com>
2020-10-27 23:35:45 +0530

    Partition set into 2 subsets with min diff-dp

--- Day47/Partition set into 2 subsets with min diff.cpp
+++ Day47/Partition set into 2 subsets with min diff.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int ans = INT_MAX;
 
+int dp[100][100][100];
+
 int find(vector<int>& v, int n, int i, int sum1, int sum2)
 {
 	if(i == v.size())
@@ -12,16 +14,33 @@ int find(vector<int>& v, int n, int i, int sum1, int sum2)
 		return ans;
 	}
 	
+	if(dp[i][sum1][sum2] != -1)
+	{
+		return dp[i][sum1][sum2];
+	}
+	
 	int a = INT_MAX;
 	
 	a = min(a, find(v, n, i + 1, sum1 + v[i], sum2));
 	a = min(a, find(v, n, i + 1, sum1, sum2 + v[i]));
 	
+	dp[i][sum1][sum2] = a;
 	return a;
 }
 
 int main()
 {
+	for(int i = 0; i < 100; i++)
+	{
+		for(int j = 0; j < 100; j++)
+		{
+			for(int k = 0; k < 100; k++)
+			{
+				dp[i][j][k] = -1;
+			}
+		}
+	}
+	
 	int n;
 	cin >> n;
 	
