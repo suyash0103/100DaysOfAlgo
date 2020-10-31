81963491fa6486982742b600c0a8751c0dc017a0
suyash0103 <suyashghuge@gmail.com>
2020-10-31 13:34:48 +0530

    word wrap

--- Day51/Word wrap.cpp
+++ Day51/Word wrap.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 #include<vector>
 #include<cmath>
+#include<string>
+
+//#include<bits/stdc++.h>
 
 using namespace std;
 
 int ans = INT_MAX;
 
-int find(string s, int m, int i, int sum)
+int find(string s, int m, int line, int i, int sum)
 {
-    cout << i << " " << sum << endl;
+    cout << i << " " << line << " " << sum << endl;
     if(i >= s.length())
     {
+//    	cout << "HERE";
         ans = min(ans, sum);
         return 1;
     }
@@ -19,27 +23,41 @@ int find(string s, int m, int i, int sum)
     int a = INT_MAX;
     while(i < s.length() && filled <= m)
     {
-        cout << "A";
-        if(s[i] == ' ')
+//        cout << "A ";
+        if(s[i] == ' ' || i == s.length() - 1)
         {
+        	if(i == s.length() - 1)
+        	{
+        		filled++;
+			}
             int q = pow((m - filled), 3);
-            a = min(a, find(s, m, i + 1, sum + q));
+            cout << "q: " << q << endl;
+            a = min(a, find(s, m, line + 1, i + 1, sum + q));
         }
+//        if(i == s.length() - 1)
+//        {
+//        	find()
+//		}
         i++;
         filled++;
     }
+    
+    if(i == s.length())
+    {
+//    	find(s, m, i, )
+	}
 
     return a;
 }
 
 int main()
 {
-    int m;
-    cin >> m;
-
     string s;
-    cin >> s;
+    getline(cin, s);
+    
+	int m;
+    cin >> m;   
 
-    cout << find(s, m, 0, 0) << endl;
+    cout << find(s, m, 1, 0, 0) << endl;
     cout << ans;
 }
