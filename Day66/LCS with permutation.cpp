f9ac83b8a531751505471e4de681b8310ddca930
suyash0103 <suyashghuge@gmail.com>
2020-11-15 11:43:55 +0530

    LCS with permutation

---
+++ Day66/LCS with permutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    map<char, int> mp1;
+    map<char, int> mp2;
+
+    for(int i = 0; i < s1.length(); i++)
+    {
+        mp1[s1[i]]++;
+    }
+
+    for(int i = 0; i < s2.length(); i++)
+    {
+        mp1[s2[i]]++;
+    }
+
+    string ans = "";
+    for(int i = 0; i < s1.length(); i++)
+    {
+        if(mp2.find(s1[i]) != mp2.end())
+        {
+            int t = min(mp2[s1[i]], mp1[s1[i]]);
+            while(t--)
+            {
+                ans.push_back(s1[i]);
+            }
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+}
