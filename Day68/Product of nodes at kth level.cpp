ad82019181f8caee1c3a2a29852df9533828737b
suyash0103 <suyashghuge@gmail.com>
2020-11-17 13:06:47 +0530

    product of nodes at kth level

---
+++ Day68/Product of nodes at kth level.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int k;
+    cin >> k;
+
+    int ans = 1;
+    int l = -1;
+
+    stack<char> st;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == '(')
+        {
+            l++;
+        }
+        else if(s[i] == ')')
+        {
+            l--;
+        }
+
+        if(l == k && s[i] >= '0' && s[i] <= '9')
+        {
+            ans = ans * (s[i] - '0');
+        }
+    }
+
+    cout << ans << endl;
+}
